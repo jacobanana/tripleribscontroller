34198016685641caa4a508f3408c5ee2ee741fe5
Adrien Fauconnet <mixedmode@mixedmode.local>
2025-03-14 01:54:03 +0100

    tidy up

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 #include <Bounce2.h>
-// #include <usbMIDI.h>
 
 #define SWITCH1_PIN 2
 #define SWITCH2_PIN 3
@@ -13,7 +12,6 @@
 Bounce2::Button switch1;
 Bounce2::Button switch2;
 Bounce2::Button switch3;
-uint8_t lastValue = 255;
 
 void setup() {
     switch1.attach(SWITCH1_PIN, INPUT_PULLUP);
@@ -24,11 +22,6 @@ void setup() {
 
     switch3.attach(SWITCH3_PIN, INPUT_PULLUP);
     switch3.interval(SWITCH_INTERVAL);
-
-}
-
-void sendMIDI(uint8_t value) {
-    usbMIDI.sendControlChange(CC_NUMBER, value, MIDI_CHANNEL);
 }
 
 void loop() {
@@ -36,30 +29,13 @@ void loop() {
     switch2.update();
     switch3.update();
     
-
-    uint8_t value = 0;
-
     if (switch1.fell()) {
         usbMIDI.sendRealTime(usbMIDI.Start);
-        value = 32; // S1
     }
     else if (switch2.fell()) {
-        value = 64; // S2
         usbMIDI.sendRealTime(usbMIDI.Stop);
     } 
     else if (switch3.fell()) {
-        value = 127; // S3
         usbMIDI.sendRealTime(usbMIDI.Continue);
-
     } 
-    // else if (switch1.rose() || switch2.rose() || switch3.rose()){
-    //     lastValue = 0;
-    // }
-    
-    // if (value != lastValue && value != 0) {
-    //     sendMIDI(value);
-    //     lastValue = value;
-    // }
-
-    // usbMIDI.read(); // Process incoming USB MIDI messages
 }
