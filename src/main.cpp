48cdec330b321bed17ed76006873158c43de7711
Adrien Fauconnet <mixedmode@mixedmode.local>
2025-03-14 03:03:55 +0100

    add LED toggle on message

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,17 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 #define SWITCH3_PIN 0
 #define MIDI_TX_PIN 1
 #define SWITCH_INTERVAL 10
+#define INTERNAL_LED 13
 
 Bounce2::Button switch1;
 Bounce2::Button switch2;
 Bounce2::Button switch3;
+bool ledState = 1;
 
 void setup() {
     MIDI.begin(MIDI_CHANNEL_OMNI);
+    pinMode(INTERNAL_LED, OUTPUT);
+    digitalWrite(INTERNAL_LED, ledState);
 
     switch1.attach(SWITCH1_PIN, INPUT_PULLUP);
     switch1.interval(SWITCH_INTERVAL);
@@ -31,6 +35,8 @@ void sendRealTimeMidi(midi::MidiType message){
     // send real time messages to both ports
     MIDI.sendRealTime(message);
     usbMIDI.sendRealTime(message);
+    ledState = !ledState;
+    digitalWrite(INTERNAL_LED, ledState);
 }
 
 void loop() {
